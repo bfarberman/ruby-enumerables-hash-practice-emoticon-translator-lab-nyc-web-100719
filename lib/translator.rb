90250aa8ece5c9ad52e259e216d2b85fab0a7816
Brad Farberman <bfarberman@gmail.com>
2019-10-04 02:03:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,8 @@ def get_japanese_emoticon(emoticon_yaml, western_emoticon)
   new_hash = load_library(emoticon_yaml)
   if new_hash[:get_emoticon].has_key?(western_emoticon) == false 
     return "sorry, that emoticon was not found"
+  else
+    
   
 end
 
