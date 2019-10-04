62a19300d5b189a1c036015fe386b4c31e9a151b
Brad Farberman <bfarberman@gmail.com>
2019-10-04 01:55:50 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,13 +11,23 @@ end
 emoticon_hash.each do |meaning, emoticons_array|
   new_hash[:get_emoticon] [emoticons_array[0]] = emoticons_array[1]
 end
-
+return new_hash
 end
+
+
+
 
 def get_japanese_emoticon
-  # code goes here
+  
 end
 
+
+
+
+
+
+
+
 def get_english_meaning
-  # code goes here
+  
 end
