cb5dac4b04b8e7d1dbcc82df5ffef8c98c43efc9
Brad Farberman <bfarberman@gmail.com>
2019-10-04 01:52:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,6 +6,7 @@ def load_library(emoticon_yaml)
   new_hash[:get_meaning] = {}
   new_hash[:get_emoticon] = {}
   emoticon_hash.each do [meaning, emoticons_array]
+  new_hash
 end
 
 def get_japanese_emoticon
