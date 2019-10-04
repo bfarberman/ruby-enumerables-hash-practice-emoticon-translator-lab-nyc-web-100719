e6f4d9ca157d043e0fabfce8de816c21bb572942
Brad Farberman <bfarberman@gmail.com>
2019-10-04 02:33:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,11 +21,11 @@ end
 
 
 def get_japanese_emoticon(yaml, western_emoticon)
-  new_hash = load_library(yaml)   
-    if new_hash[:get_emoticon].has_key?(western_emoticon) == false
+  n_hash = load_library(yaml)   
+    if n_hash[:get_emoticon].has_key?(western_emoticon) == false
       return "Sorry, that emoticon was not found"
     else
-      new_hash.each do |get_key, translator_hash|
+      n_hash.each do |get_key, translator_hash|
         translator_hash.each do |english_emoticon, japanese_emoticon|
           if english_emoticon == western_emoticon
             return japanese_emoticon
@@ -40,11 +40,11 @@ end
 
 
 def get_english_meaning(yaml, other_emoticon)
-  new_hash = load_library(yaml)
-    if new_hash[:get_meaning].has_key?(other_emoticon) == false
+  n_hash = load_library(yaml)
+    if n_hash[:get_meaning].has_key?(other_emoticon) == false
       return "Sorry, that emoticon was not found"
     else
-      new_hash.each do |get_key, translator_hash|
+      n_hash.each do |get_key, translator_hash|
         translator_hash.each do |japanese_emoticon, english_meaning|
           if japanese_emoticon == other_emoticon
             return english_meaning
